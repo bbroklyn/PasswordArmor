826a49964463028913d5aa5370cb537f521f0823
Vlad Konovalow <100339728+bbroklyn@users.noreply.github.com>
2024-05-18 22:37:39 +0500

    Beginning of work

---
+++ src/module/password.module.ts
@@ -0,0 +1,9 @@
+export const validationConfig = {
+	regex: {
+		digits: '(\\d.*)',
+		letters: '([a-zA-Z].*)',
+		symbols:
+			'([`~\\!@#\\$%\\^\\&\\*\\(\\)\\-_\\=\\+\\[\\{\\}\\]\\\\|;:\\\'",<.>\\/\\?€£¥₹§±].*)',
+		spaces: '([\\s].*)',
+	},
+}
---
+++ src/validator.ts
@@ -0,0 +1,1 @@
+export function ifStrong() {}
